314bea291cd90b0d912755a704f8f1b686c598fd
donniean <acmilank22@gmail.com>
2022-03-31 14:56:48 +0800

    feat: modified config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -2,16 +2,7 @@ const { merge } = require('webpack-merge');
 
 const baseConfig = require('./docusaurus.config.base');
 const tkeelConfig = require('./docusaurus.config.tkeel');
-const qingcloudConfig = require('./docusaurus.config.qingcloud');
 
-const configType = 'tkeel'; // tkeel, qingcloud
-
-let config = null;
-
-if (configType === 'qingcloud') {
-  config = merge({}, baseConfig, qingcloudConfig);
-} else {
-  config = merge({}, baseConfig, tkeelConfig);
-}
+const config = merge({}, baseConfig, tkeelConfig);
 
 module.exports = config;

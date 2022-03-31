1f433341a09b684912c2073791b083b3ab67013d
donniean <acmilank22@gmail.com>
2022-03-31 16:27:45 +0800

    feat: add linters

---
+++ .eslintrc.js
@@ -0,0 +1,131 @@
+module.exports = {
+  root: true,
+  parser: '@babel/eslint-parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    requireConfigFile: false,
+  },
+  env: {
+    browser: true,
+    node: true,
+    commonjs: true,
+    'shared-node-browser': true,
+    amd: true,
+    es6: true,
+    es2017: true,
+    es2020: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb',
+    'airbnb/hooks',
+    'plugin:eslint-comments/recommended',
+    'plugin:promise/recommended',
+    'plugin:unicorn/recommended',
+    'plugin:sonarjs/recommended',
+    'plugin:prettier/recommended',
+  ],
+  rules: {
+    'no-console': process.env.NODE_ENV === 'development' ? 'warn' : 'error',
+    'no-param-reassign': [
+      'error',
+      {
+        props: true,
+        ignorePropertyModificationsFor: [
+          'acc',
+          'accumulator',
+          'e',
+          'ctx',
+          'context',
+          'req',
+          'request',
+          'res',
+          'response',
+          '$scope',
+          'staticContext',
+          'draft',
+        ],
+      },
+    ],
+    'no-useless-call': 'error',
+    'import/no-duplicates': ['error', { considerQueryString: true }],
+    'import/order': [
+      'error',
+      {
+        groups: [
+          'builtin',
+          'external',
+          'internal',
+          ['parent', 'sibling', 'index'],
+          'object',
+          'type',
+          'unknown',
+        ],
+        'newlines-between': 'always',
+        alphabetize: {
+          order: 'asc',
+        },
+        warnOnUnassignedImports: true,
+      },
+    ],
+    'unicorn/filename-case': [
+      'error',
+      {
+        cases: {
+          kebabCase: true,
+          camelCase: true,
+          pascalCase: true,
+        },
+      },
+    ],
+    'unicorn/no-array-for-each': 'off',
+    'unicorn/no-null': 'off',
+    'unicorn/prevent-abbreviations': 'off',
+    'react/jsx-key': 'error',
+    'react/jsx-props-no-spreading': 'off',
+    'react/jsx-uses-react': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/require-default-props': [
+      'error',
+      { forbidDefaultForRequired: true, ignoreFunctionalComponents: true },
+    ],
+  },
+  overrides: [
+    {
+      files: ['**/*.js'],
+      rules: {
+        'import/no-extraneous-dependencies': 'off',
+        'unicorn/prefer-module': 'off',
+      },
+    },
+    {
+      files: ['**/*.{ts,tsx}'],
+      parserOptions: {
+        project: './tsconfig.json',
+      },
+      settings: {
+        'import/resolver': {
+          typescript: {
+            alwaysTryTypes: true,
+          },
+        },
+      },
+      extends: [
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+        'airbnb-typescript',
+        'plugin:prettier/recommended',
+      ],
+      plugins: ['simple-import-sort'],
+      rules: {
+        'sort-imports': 'off',
+        'import/order': 'off',
+        'simple-import-sort/imports': 'error',
+        'simple-import-sort/exports': 'error',
+        '@typescript-eslint/ban-ts-comment': 'off',
+        '@typescript-eslint/no-floating-promises': 'off',
+      },
+    },
+  ],
+};
